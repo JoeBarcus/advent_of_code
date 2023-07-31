5059903ec40d7fe3f9a20315f5df1ffc574fc568
JoeBarcus <joebarcus981@gmail.com>
2023-07-30 18:18:57 -0600

    Rust Day two part two

--- rust/day_two/src/main.rs
+++ rust/day_two/src/main.rs
@@ -10,14 +10,25 @@ fn main() {
 
     let mut totals: Vec<i32> = Vec::new();
     
-    for i in collection {
+    for i in collection.clone() {
         let total_play = match_map(i);
         totals.push(total_play);
     }
 
     let max: i32 = totals.iter().sum();
 
-    println!("{:?}", max)
+    println!("Part One {:?}", max);
+
+    let mut totals_two: Vec<i32> = Vec::new();
+
+    for i in collection.clone() {
+        let total_play = match_map_two(i);
+        totals_two.push(total_play);
+    }
+
+    let max_two: i32 = totals_two.iter().sum();
+
+    println!("Part One {:?}", max_two);
 
 }
 
@@ -42,6 +53,33 @@ fn match_map(plays: &str) -> i32 {
         "C Y" => 2,
         // Scissors Scissors Tie 3 3
         "C Z" => 6,
+        _ => {
+            println!("Invalid Input");
+            -1
+        }
+    }
+}
+
+fn match_map_two(plays: &str) -> i32 {
+    match plays {
+        // Rock Scissors Lose 3 0
+        "A X" => 3,
+        // Rock Rock Tie 1 3
+        "A Y" => 4,
+        // Rock Paper Win 2 6
+        "A Z" => 8,
+        // Paper Rock Lose 1 0
+        "B X" => 1,
+        // Paper Paper Tie 2 3
+        "B Y" => 5,
+        // Paper Scissors Win 3 6
+        "B Z" => 9,
+        // Scissors Paper Lose 2 0
+        "C X" => 2,
+        // Scissors Scissors Tie 3 2
+        "C Y" => 6,
+        // Scissors Rock Win 1 6
+        "C Z" => 7,
         _ => {
             println!("Invalid Input");
             -1
