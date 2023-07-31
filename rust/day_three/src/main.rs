d6c3f84789e05c5142cdbd7f6bbeb3b039fbca4d
JoeBarcus <joebarcus981@gmail.com>
2023-07-31 13:24:49 -0600

    Day 3 part 2 rust

--- rust/day_three/src/main.rs
+++ rust/day_three/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     
 
     let mut rucksacks: Vec<(&str, &str)> = Vec::new();
-    for i in collection {
+    for i in collection.clone() {
         let midpoint: usize = i.len() / 2;
         let end: usize = i.len();
         
@@ -37,7 +37,34 @@ fn main() {
 
     let total: i32 = common_items.iter().map(|&x| x).sum();
     println!("{}", total);
-}
+
+
+
+    /////////////////  Part 2  /////////////////
+    
+    let mut letter_value: Vec<&i32> = Vec::new();
+
+
+    for ruck in collection.chunks(3) {
+        let ruck_one: HashSet<_> = ruck[0].chars().collect();
+        let ruck_two: HashSet<_> = ruck[1].chars().collect();
+        let ruck_three: HashSet<_> = ruck[2].chars().collect();
+        
+        let common_letters = ruck_one.intersection(&ruck_two).cloned().collect::<HashSet<char>>().intersection(&ruck_three).cloned().collect::<HashSet<char>>();
+        let char_conversion = *common_letters.iter().next().unwrap();
+        let mapping_value: &i32 = mappings.get(&char_conversion).unwrap();
+        
+        letter_value.push(mapping_value)
+
+    }
+
+    let total_part_two: i32 = letter_value.iter().map(|&x| x).sum();
+    println!("{:?}", total_part_two);
+
+    }
+
+
+
 
 
 pub fn letters() -> Vec<char> {
