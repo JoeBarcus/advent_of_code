0dced4d530cbc835eaeded437d1b3e3209ef1b28
JoeBarcus <joebarcus981@gmail.com>
2023-07-31 17:02:26 -0600

    Forgot to save python

--- python/day_four/solution.py
+++ python/day_four/solution.py
@@ -32,29 +32,3 @@ for camp in camp_cleanup:
 
 print(overlap)
 print(overlap_two)
-
-#############  Part 2  #############
-
-
-def is_between(value, lower_bound, upper_bound):
-    return lower_bound <= value <= upper_bound
-
-overlap = 0
-for camp in camp_cleanup:
-    split_camp = camp.split(',')
-    split_camp_one = split_camp[0].split('-')
-    split_camp_one = [int(num) for num in split_camp_one]
-    split_camp_two = split_camp[1].split('-')
-    split_camp_two = [int(num) for num in split_camp_two]
-
-    if is_between(split_camp_one[0], split_camp_two[0], split_camp_two[1]):
-        overlap += 1
-    elif is_between(split_camp_one[1], split_camp_two[0], split_camp_two[1]):
-        overlap += 1
-    elif is_between(split_camp_two[0], split_camp_one[0], split_camp_one[1]):
-        overlap += 1
-    elif is_between(split_camp_two[1], split_camp_one[0], split_camp_one[1]):
-        overlap += 1
-
-
-
