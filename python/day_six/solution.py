5f00638b8532ea089ce0855624c79ea0e421e5e3
JoeBarcus <joebarcus981@gmail.com>
2023-08-05 17:19:52 -0600

    Day 6 part 1

---
+++ python/day_six/solution.py
@@ -0,0 +1,18 @@
+with open ('input.txt', 'r') as input_data:
+    signal_packet = input_data.read()
+    input_data.close()
+
+consecutive_counter_set = set()
+
+for i, letter in enumerate(signal_packet):
+    if letter in consecutive_counter_set:
+        consecutive_counter_set = set()
+        continue
+    consecutive_counter_set.add(letter)
+    
+    if len(consecutive_counter_set) == 4:
+        packet = i
+        break
+
+
+print(packet)
